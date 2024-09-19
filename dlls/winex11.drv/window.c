269d426cce805353547e0c4bc45eb620b2b1da17
Paul Gofman <pgofman@codeweavers.com>
2024-09-19 17:45:28 -0600

    winex11.drv: Set X window property to indicate if the presentation may flip.
    CW-Bug-Id: #24273

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -1927,6 +1927,8 @@ Window get_dummy_parent(void)
  */
 void detach_client_window( struct x11drv_win_data *data, Window client_window, BOOL reparent )
 {
+    unsigned int allow_flip = 0;
+
     if (data->client_window != client_window || !client_window) return;
     data->client_window = 0;
 
@@ -1936,6 +1938,8 @@ void detach_client_window( struct x11drv_win_data *data, Window client_window, B
     XFlush( data->display ); /* make sure XSelectInput is disabled for client_window after this point */
     XDeleteContext( data->display, client_window, winContext );
 
+    XChangeProperty( gdi_display, client_window, x11drv_atom(_WINE_ALLOW_FLIP), XA_CARDINAL, 32,
+                     PropModeReplace, (unsigned char *)&allow_flip, sizeof(allow_flip) / 4 );
     if (reparent) XReparentWindow( gdi_display, client_window, get_dummy_parent(), 0, 0 );
     TRACE( "%p/%lx detached client window %lx\n", data->hwnd, data->whole_window, client_window );
 }
@@ -1946,6 +1950,8 @@ void detach_client_window( struct x11drv_win_data *data, Window client_window, B
  */
 void attach_client_window( struct x11drv_win_data *data, Window client_window )
 {
+    unsigned int allow_flip = 1;
+
     if (data->client_window == client_window || !client_window) return;
     detach_client_window( data, data->client_window, TRUE );
     data->client_window = client_window;
@@ -1956,8 +1962,11 @@ void attach_client_window( struct x11drv_win_data *data, Window client_window )
     XSelectInput( data->display, client_window, ExposureMask );
     XFlush( data->display ); /* make sure XSelectInput is enabled for client_window after this point */
 
+    XChangeProperty( gdi_display, client_window, x11drv_atom(_WINE_ALLOW_FLIP), XA_CARDINAL, 32,
+                     PropModeReplace, (unsigned char *)&allow_flip, sizeof(allow_flip) / 4 );
     XReparentWindow( gdi_display, client_window, data->whole_window, data->client_rect.left - data->whole_rect.left,
                      data->client_rect.top - data->whole_rect.top );
+
     TRACE( "%p/%lx attached client window %lx\n", data->hwnd, data->whole_window, client_window );
 }
 
@@ -2108,6 +2117,7 @@ void set_gamescope_overlay_prop( Display *display, Window window, HWND hwnd )
  */
 static void create_whole_window( struct x11drv_win_data *data )
 {
+    unsigned int allow_flip = 0;
     int cx, cy, mask;
     XSetWindowAttributes attr;
     WCHAR text[1024];
@@ -2161,6 +2171,9 @@ static void create_whole_window( struct x11drv_win_data *data )
                                         cx, cy, 0, data->vis.depth, InputOutput,
                                         data->vis.visual, mask, &attr );
     if (!data->whole_window) goto done;
+
+    XChangeProperty( data->display, data->whole_window, x11drv_atom(_WINE_ALLOW_FLIP), XA_CARDINAL, 32,
+                     PropModeReplace, (unsigned char *)&allow_flip, sizeof(allow_flip) / 4 );
 
     X11DRV_XInput2_Enable( data->display, data->whole_window, attr.event_mask );
     set_initial_wm_hints( data->display, data->whole_window );
