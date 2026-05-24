nanomatters/wine-salkim

winewayland: Use passthrough for some colorspaces.
Required for HDR to look correct on wayland since Windows works in a different way compared to wayland.

dlls/winewayland.drv/vulkan.c
@@ -130,7 +130,38 @@ static void wayland_map_device_extensions(struct vulkan_device_extensions *exten
 
 static VkResult wayland_vulkan_colorspace_configure(VkColorSpaceKHR *colorspace, struct client_surface *client)
 {
+    struct wp_image_description_v1 *wp_image_description_v1 = NULL;
+    struct wayland_client_surface *surface =
+        CONTAINING_RECORD(client, struct wayland_client_surface, client);
+    VkColorSpaceKHR old = *colorspace;
+
+    if (process_wayland.supports_scrgb &&
+        old == VK_COLOR_SPACE_EXTENDED_SRGB_LINEAR_EXT)
+    {
+        *colorspace = VK_COLOR_SPACE_PASS_THROUGH_EXT;
+        wp_image_description_v1 =
+            wp_color_manager_v1_create_windows_scrgb(process_wayland.wp_color_manager_v1);
+
+        if (!wp_image_description_v1) goto err;
+    }
+    else if (process_wayland.supports_win_pq &&
+             old == VK_COLOR_SPACE_HDR10_ST2084_EXT)
+    {
+        *colorspace = VK_COLOR_SPACE_PASS_THROUGH_EXT;
+        wp_image_description_v1 =
+            wp_color_manager_v1_create_windows_bt2100(process_wayland.wp_color_manager_v1);
+
+        if (!wp_image_description_v1) goto err;
+    }
+
+    TRACE("mapping colorspace %u => %u\n", old, *colorspace);
+
+    wayland_client_surface_attach_image_description(surface, wp_image_description_v1);
+
     return VK_SUCCESS;
+err:
+    ERR("Failed to configure image description for client surface!\n");
+    return VK_ERROR_OUT_OF_HOST_MEMORY;
 }
 
 static const struct vulkan_driver_funcs wayland_vulkan_driver_funcs =

dlls/winewayland.drv/wayland_surface.c
@@ -1322,6 +1322,8 @@ static void wayland_client_surface_destroy(struct client_surface *client)
 
     TRACE("%s\n", debugstr_client_surface(client));
 
+    if (surface->wp_color_management_surface_v1)
+        wp_color_management_surface_v1_destroy(surface->wp_color_management_surface_v1);
     if (surface->wp_content_type_v1)
         wp_content_type_v1_destroy(surface->wp_content_type_v1);
     if (surface->wp_viewport)
@@ -1487,6 +1489,74 @@ void wayland_client_surface_attach(struct wayland_client_surface *client, HWND t
     wayland_surface_reconfigure_client(surface, client, &client_rect);
     /* Commit to apply subsurface positioning. */
     wl_surface_commit(surface->wl_surface);
+}
+
+static void wayland_image_description_v1_failed(void *user_data,
+                    struct wp_image_description_v1 *wp_image_description_v1,
+                    uint32_t cause, const char *msg)
+{
+    struct wayland_client_surface *surface = user_data;
+    ERR("cause=%u msg=%s\n", cause, debugstr_a(msg));
+
+    wp_image_description_v1_destroy(wp_image_description_v1);
+    client_surface_release(&surface->client);
+}
+
+static void wayland_image_description_v1_ready2(void *user_data,
+                    struct wp_image_description_v1 *wp_image_description_v1,
+                    uint32_t identity_hi, uint32_t identity_lo)
+{
+    struct wayland_client_surface *surface = user_data;
+    TRACE("id=%#x%x\n", identity_hi, identity_lo);
+
+    if (!surface->wp_color_management_surface_v1)
+    {
+        surface->wp_color_management_surface_v1 =
+            wp_color_manager_v1_get_surface(process_wayland.wp_color_manager_v1,
+                                            surface->wl_surface);
+        if (!surface->wp_color_management_surface_v1)
+        {
+            ERR("Failed to create color management surface for client surface!\n");
+            return;
+        }
+    }
+    wp_color_management_surface_v1_set_image_description(
+        surface->wp_color_management_surface_v1,
+        wp_image_description_v1,
+        WP_COLOR_MANAGER_V1_RENDER_INTENT_PERCEPTUAL);
+    wp_image_description_v1_destroy(wp_image_description_v1);
+    client_surface_release(&surface->client);
+}
+
+static void wayland_image_description_v1_ready(void *user_data,
+                    struct wp_image_description_v1 *wp_image_description_v1,
+                    uint32_t identity)
+{
+    wayland_image_description_v1_ready2(user_data, wp_image_description_v1, 0, identity);
+}
+
+static const struct wp_image_description_v1_listener image_description_listener = {
+    wayland_image_description_v1_failed,
+    wayland_image_description_v1_ready,
+    wayland_image_description_v1_ready2
+};
+
+void wayland_client_surface_attach_image_description(struct wayland_client_surface *surface,
+                                                     struct wp_image_description_v1 *image_desc)
+{
+    if (!image_desc)
+    {
+        if (surface->wp_color_management_surface_v1)
+        {
+            wp_color_management_surface_v1_destroy(surface->wp_color_management_surface_v1);
+            surface->wp_color_management_surface_v1 = NULL;
+        }
+        return;
+    }
+
+    client_surface_add_ref(&surface->client);
+    wp_image_description_v1_add_listener(image_desc, &image_description_listener, surface);
+    wl_display_flush(process_wayland.wl_display);
 }
 
 static void dummy_buffer_release(void *data, struct wl_buffer *buffer)

CachyOS/wine-cachyos

dlls/winewayland.drv/vulkan.c
@@ -144,7 +144,36 @@ static void wayland_map_device_extensions(struct vulkan_device_extensions *exten
 
 static VkResult wayland_vulkan_colorspace_configure(VkColorSpaceKHR *colorspace, struct client_surface *client)
 {
+    struct wp_image_description_v1 *wp_image_description_v1 = NULL;
+    VkColorSpaceKHR old = *colorspace;
+
+    if (process_wayland.supports_scrgb &&
+        old == VK_COLOR_SPACE_EXTENDED_SRGB_LINEAR_EXT)
+    {
+        *colorspace = VK_COLOR_SPACE_PASS_THROUGH_EXT;
+        wp_image_description_v1 =
+            wp_color_manager_v1_create_windows_scrgb(process_wayland.wp_color_manager_v1);
+
+        if (!wp_image_description_v1) goto err;
+    }
+    else if (process_wayland.supports_win_pq &&
+             old == VK_COLOR_SPACE_HDR10_ST2084_EXT)
+    {
+        *colorspace = VK_COLOR_SPACE_PASS_THROUGH_EXT;
+        wp_image_description_v1 =
+            wp_color_manager_v1_create_windows_bt2100(process_wayland.wp_color_manager_v1);
+
+        if (!wp_image_description_v1) goto err;
+    }
+
+    TRACE("mapping colorspace %u => %u\n", old, *colorspace);
+
+    wayland_client_surface_attach_image_description(client, wp_image_description_v1);
+
     return VK_SUCCESS;
+err:
+    ERR("Failed to configure image description for client surface!\n");
+    return VK_ERROR_OUT_OF_HOST_MEMORY;
 }
 
 static const struct vulkan_driver_funcs wayland_vulkan_driver_funcs =

dlls/winewayland.drv/wayland_surface.c
@@ -1322,6 +1322,8 @@ static void wayland_client_surface_destroy(struct client_surface *client)
 
     TRACE("%s\n", debugstr_client_surface(client));
 
+    if (surface->wp_color_management_surface_v1)
+        wp_color_management_surface_v1_destroy(surface->wp_color_management_surface_v1);
     if (surface->wp_content_type_v1)
         wp_content_type_v1_destroy(surface->wp_content_type_v1);
     if (surface->wp_viewport)
@@ -1487,6 +1489,75 @@ void wayland_client_surface_attach(struct wayland_client_surface *client, HWND t
     wayland_surface_reconfigure_client(surface, client, &client_rect);
     /* Commit to apply subsurface positioning. */
     wl_surface_commit(surface->wl_surface);
+}
+
+static void wayland_image_description_v1_failed(void *user_data,
+                    struct wp_image_description_v1 *wp_image_description_v1,
+                    uint32_t cause, const char *msg)
+{
+    struct wayland_client_surface *surface = user_data;
+    ERR("cause=%u msg=%s\n", cause, debugstr_a(msg));
+
+    wp_image_description_v1_destroy(wp_image_description_v1);
+    client_surface_release(&surface->client);
+}
+
+static void wayland_image_description_v1_ready2(void *user_data,
+                    struct wp_image_description_v1 *wp_image_description_v1,
+                    uint32_t identity_hi, uint32_t identity_lo)
+{
+    struct wayland_client_surface *surface = user_data;
+    TRACE("id=%#x%x\n", identity_hi, identity_lo);
+
+    if (!surface->wp_color_management_surface_v1)
+    {
+        surface->wp_color_management_surface_v1 =
+            wp_color_manager_v1_get_surface(process_wayland.wp_color_manager_v1,
+                                            surface->wl_surface);
+        if (!surface->wp_color_management_surface_v1)
+        {
+            ERR("Failed to create color management surface for client surface!\n");
+            return;
+        }
+    }
+    wp_color_management_surface_v1_set_image_description(
+        surface->wp_color_management_surface_v1,
+        wp_image_description_v1,
+        WP_COLOR_MANAGER_V1_RENDER_INTENT_PERCEPTUAL);
+    wp_image_description_v1_destroy(wp_image_description_v1);
+    client_surface_release(&surface->client);
+}
+
+static void wayland_image_description_v1_ready(void *user_data,
+                    struct wp_image_description_v1 *wp_image_description_v1,
+                    uint32_t identity)
+{
+    wayland_image_description_v1_ready2(user_data, wp_image_description_v1, 0, identity);
+}
+
+static const struct wp_image_description_v1_listener image_description_listener = {
+    wayland_image_description_v1_failed,
+    wayland_image_description_v1_ready,
+    wayland_image_description_v1_ready2
+};
+
+void wayland_client_surface_attach_image_description(struct client_surface *client,
+                                                     struct wp_image_description_v1 *image_desc)
+{
+    struct wayland_client_surface *surface = impl_from_client_surface(client);
+    if (!image_desc)
+    {
+        if (surface->wp_color_management_surface_v1)
+        {
+            wp_color_management_surface_v1_destroy(surface->wp_color_management_surface_v1);
+            surface->wp_color_management_surface_v1 = NULL;
+        }
+        return;
+    }
+
+    client_surface_add_ref(&surface->client);
+    wp_image_description_v1_add_listener(image_desc, &image_description_listener, surface);
+    wl_display_flush(process_wayland.wl_display);
 }
 
 static void dummy_buffer_release(void *data, struct wl_buffer *buffer)